657048d329de8e9d145b18424fa3c4900e56ddd9
xExoDuzZ <87362153+xSteeVen@users.noreply.github.com>
2022-07-06 16:36:58 +0200

    first commit

---
+++ src/test/java/com/steven/spring/mongo/api/springmongodb/SpringMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steven.spring.mongo.api.springmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
