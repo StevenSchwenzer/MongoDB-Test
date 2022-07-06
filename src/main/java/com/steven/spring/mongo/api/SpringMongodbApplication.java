c8896f622769b446811d3d0e636f4057ae247f88
Riccardo Bottini <riccardo.bottini@external.eni.com>
2022-07-06 16:56:47 +0200

    fixed some issues

--- src/main/java/com/steven/spring/mongo/api/SpringMongodbApplication.java
+++ src/main/java/com/steven/spring/mongo/api/SpringMongodbApplication.java
@@ -1,4 +1,4 @@
-package com.steven.spring.mongo.api.springmongodb;
+package com.steven.spring.mongo.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
