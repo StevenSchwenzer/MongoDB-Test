563c446670de2428bf5891857c6b28aa383bc27f
Riccardo Bottini <riccardo.bottini@external.eni.com>
2022-07-07 12:21:29 +0200

    fixed

--- src/main/java/com/steven/spring/mongo/api/config/SwaggerConfig.java
+++ src/main/java/com/steven/spring/mongo/api/config/SwaggerConfig.java
@@ -1,24 +1,23 @@
 package com.steven.spring.mongo.api.config;
-
+import com.google.common.base.Predicate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.google.common.base.Predicate;
-
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-//@Configuration
+import static com.google.common.base.Predicates.or;
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-        
-        private static final String TITLE = "MongoDB-Test SpringBoot";
 
-        private static final String DESCRIPTION = "Just Testing Microservice with MongoDB";
-
+        private static final String TITLE = "Booking-Microservice API";
+        private static final String DESCRIPTION = "Microservice for determining bookings in the context "
+                + "of an ending charging request";
         private static final String VERSION = "1.0.0";
 
         @Bean
@@ -31,13 +30,21 @@ public class SwaggerConfig {
                         .build();
         }
 
-        private Predicate<String> postPaths() { return or(regex("api/posts.*"), regex("api/.*"));}
+        private Predicate<String> postPaths() {
+                return or(regex("/api/posts.*"), regex("/api/.*"));
+        }
+
+
 
         @SuppressWarnings("deprecation")
         private ApiInfo apiInfo() {
                 return new ApiInfoBuilder()
                         .title(TITLE)
                         .description(DESCRIPTION)
+                        //.termsOfServiceUrl("https://www.my-oli.com/en/")
+                        //.contact("maurizio.bono@my-oli.com")
+                        //.license("OLI Systems License")
+                        //.licenseUrl("muhammad.yahya@my-oli.com")
                         .version(VERSION)
                         .build();
         }
